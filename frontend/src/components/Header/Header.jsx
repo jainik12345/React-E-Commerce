22baaecddb40b1bddcf02a10cbb76fd49f5ce7fb
Jainik Darji <darjijainik@gmail.com>
2024-06-29 19:27:52 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Food </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dicta
+          asperiores maiores nam ratione amet magni voluptates id incidunt,
+          reprehenderit laborum culpa ducimus, molestiae tenetur doloremque
+          placeat odit unde earum?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
